7e0782d8b72a5510dab9cb583640c1bd9506e1b0
Keshawn Clark <Keshawn549@gmail.com>
2023-04-20 17:52:52 -0400

    a program that creates a band name for you
    using where you grew up at and the name of your pet to make you a band name! my first every project :)

---
+++ main.py
@@ -0,0 +1,10 @@
+#1. Create a greeting for your program.
+print("uhm, pizza rolls? haha so quirky so funny.")
+#2. Ask the user for the city that they grew up in.
+city = (
+    input("where did u grow up?"))
+#3. Ask the user for the name of a pet.
+pet = (input(
+    "what's the name of your pet?"))
+# 4. Combine the name of their city and pet and show them their band name.
+print("your band name could be " + city + "" + pet)
